1cf888875ed1bf823328152256c4de8cd77da770
rafale77 <anhmantran@yahoo.com>
2020-03-14 22:31:16 -0700

    Update L_ZWay2.lua

--- L_ZWay2.lua
+++ L_ZWay2.lua
@@ -2,7 +2,7 @@ module (..., package.seeall)
 
 ABOUT = {
   NAME          = "L_ZWay2",
-  VERSION       = "2020.03.14",
+  VERSION       = "2020.03.15",
   DESCRIPTION   = "Z-Way interface for openLuup",
   AUTHOR        = "@akbooer",
   COPYRIGHT     = "(c) 2013-2020 AKBooer",
